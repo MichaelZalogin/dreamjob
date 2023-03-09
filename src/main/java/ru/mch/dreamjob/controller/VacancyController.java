785b504163a94bfd4da7f821fa5b3cc37c03fe60
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-10 01:40:00 +0300

    Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/mch/dreamjob/controller/VacancyController.java
@@ -0,0 +1,21 @@
+package ru.mch.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.mch.dreamjob.repository.MemoryVacancyRepository;
+import ru.mch.dreamjob.repository.VacancyRepository;
+
+@Controller
+@RequestMapping("/vacancies")
+public class VacancyController {
+
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("vacancies", vacancyRepository.findAll());
+        return "vacancies/list";
+    }
+}
