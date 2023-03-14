4d039009d24911f6dec68b9cfd1db17ec91c6a5e
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-14 03:53:55 +0300

    Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/mch/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/mch/dreamjob/controller/CandidateController.java
@@ -2,8 +2,9 @@ package ru.mch.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.mch.dreamjob.entity.Candidate;
+import ru.mch.dreamjob.entity.Vacancy;
 import ru.mch.dreamjob.repository.CandidateRepository;
 import ru.mch.dreamjob.repository.MemoryCandidateRepository;
 
@@ -22,5 +23,42 @@ public class CandidateController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "candidates/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Candidate candidate) {
+        candidateRepository.save(candidate);
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var candidateOptional = candidateRepository.findById(id);
+        if (candidateOptional.isEmpty()) {
+            model.addAttribute("message", "Резюме с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        model.addAttribute("candidate", candidateOptional.get());
+        return "candidates/one";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Candidate candidate, Model model) {
+        var isUpdated = candidateRepository.update(candidate);
+        if (!isUpdated) {
+            model.addAttribute("message", "Резюме с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = candidateRepository.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Резюме с указанным идентификатором не найдено");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
     }
 }
