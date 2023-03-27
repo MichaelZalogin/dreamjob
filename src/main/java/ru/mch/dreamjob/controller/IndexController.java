c8da46f324a19a1bba71f86537203239d5c58420
MichaelZalogin <zaloginmiha@gmail.com>
2023-03-27 06:02:02 +0300

    index.html [#504863]

--- src/main/java/ru/mch/dreamjob/controller/IndexController.java
+++ src/main/java/ru/mch/dreamjob/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+    // TODO: 27.03.2023 http://dreamjobmch.ru domen 
     @GetMapping("/index")
     public String getIndex() {
         return "index";
